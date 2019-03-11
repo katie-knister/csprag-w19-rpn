8ff8e1683c1e4789a8b8adcd6dae927eebb3ce65
Katie Knister <kknister@umich.edu>
2019-03-11 13:19:38 -0400

    Changed rpn.py and test_prn.py to include the given code. Added exponent test to test_rpn.py.

--- rpn.py
+++ rpn.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
 
-def calculate(arg):
-    stack = []
-    tokens = arg.split()
-    print(tokens)
+import operator
+
+
+operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+}
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
+   # tokens = arg.split()
+   # print(tokens)
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
