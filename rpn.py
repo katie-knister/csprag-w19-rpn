88bc06f0d6b355d1b3398e0a0e6278ad78e2099b
Katie Knister <kknister@umich.edu>
2019-03-11 10:26:59 -0400

    Initial commit

---
+++ rpn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+    stack = []
+    tokens = arg.split()
+    print(tokens)
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()
