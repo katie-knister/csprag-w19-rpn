8cf116c8a01e129f9d77683e34f191bcccb38f10
Katie Knister <kknister@umich.edu>
2019-03-13 11:40:20 -0400

    Another attempt to get colors to work

--- rpn.py
+++ rpn.py
@@ -4,7 +4,7 @@ import operator
 
 import readline
 
-#from colored import fg, bg
+from colored import fg, bg
 
 operators = {
         '+': operator.add,
@@ -16,7 +16,7 @@ operators = {
        # '-': operator.neg,
 }
 def calculate(myarg):
-    #colors = bg('rosy_brown') + fg('sea_green_2')
+    colors = bg('rosy_brown') + fg('sea_green_2')
     stack = list()
     for token in myarg.split():
         try:
@@ -28,8 +28,8 @@ def calculate(myarg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-            print(stack)
-#        print(colors + stack)
+           # print(stack)
+            print(colors + stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
